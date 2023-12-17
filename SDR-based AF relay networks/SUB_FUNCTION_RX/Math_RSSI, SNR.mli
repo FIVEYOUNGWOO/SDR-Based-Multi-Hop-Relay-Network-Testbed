441e95d891938d5dfe6cc5bf049eeb8a14b01401
Youngwoo Oh <96539912+FIVEYOUNGWOO@users.noreply.github.com>
2023-12-17 16:17:46 +0900

    Add files via upload

---
+++ SDR-based AF relay networks/SUB_FUNCTION_RX/Math_RSSI, SNR.mli
@@ -0,0 +1,105 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<SourceFile Checksum="649D7FD6D46A0C146262DD86EFF68659D3F309B14C43852D9686B555D79B22225FB1383CBE319274CE7F49597898713BE7031272E80C69664AE429CF2AA55646" Timestamp="1D6808E032B63D3" xmlns="http://www.ni.com/PlatformFramework">
+	<SourceModelFeatureSet>
+		<ParsableNamespace AssemblyFileVersion="8.1.0.50414" FeatureSetName="MATLAB®용 인터페이스" Name="http://www.ni.com/Matlab" OldestCompatibleVersion="5.3.1.49152" Version="5.3.1.49152" />
+		<ParsableNamespace AssemblyFileVersion="8.1.0.50414" FeatureSetName="편집기" Name="http://www.ni.com/PlatformFramework" OldestCompatibleVersion="8.1.0.49152" Version="8.1.0.49152" />
+		<ApplicationVersionInfo Build="8.1.0.50414" Name="LabVIEW NXG" Version="5.0.0" />
+	</SourceModelFeatureSet>
+	<MatlabDefinition Id="b0c2b8ad479840299f6a45a6adf95398" xmlns="http://www.ni.com/Matlab">
+		<p.PlatformAgnosticPathToLibrary>
+			<Path a="1">
+				<pE>C</pE>
+				<pE>Users</pE>
+				<pE>duddn</pE>
+				<pE>OneDrive</pE>
+				<pE>바탕 화면</pE>
+				<pE>MathScript_2.m</pE>
+			</Path>
+		</p.PlatformAgnosticPathToLibrary>
+		<Icon Id="560530ad8f3c45679cd43e895fb78260" ListViewIconCrop="0 0 40 40" xmlns="http://www.ni.com/PlatformFramework">
+			<IconPanel Height="[float]40" Id="c0e0d3355a5342afa8567686cb4f6855" Left="[float]0" PanelSizeMode="Resize" Top="[float]0" Width="[float]40">
+				<IconTemplate ClipMargin="[SMThickness]3,3,3,3" Height="[float]40" HorizontalAlignment="[HorizontalAlignment]Stretch" Id="dc962ed2a10647beb6b2b6412b28c1c3" Left="[float]0" TemplateName="[string]Corner" Top="[float]0" VerticalAlignment="[VerticalAlignment]Stretch" Width="[float]40">
+					<Path Data="[PathGeometry]M 0,0 C 0,1.10000002 0.899999976,2 2,2 L 2,0 L 0,0 Z" Fill="[SMSolidColorBrush]#ff00bff3" Height="[float]2" HorizontalAlignment="[HorizontalAlignment]Left" Id="44d7caea86ef4010a19958f100bcf993" IsHitTestVisible="[bool]False" Left="[float]0" Margin="[SMThickness]2,2,2,2" MinHeight="[float]1" MinWidth="[float]1" Top="[float]0" VerticalAlignment="[VerticalAlignment]Bottom" Width="[float]2" />
+					<Rectangle Fill="[SMSolidColorBrush]#ff00bff3" Height="[float]2" Id="2799019d5bb947a1b3fa32fa01a7c8f9" IsHitTestVisible="[bool]False" Left="[float]0" Margin="[SMThickness]4,2,4,2" MinHeight="[float]1" MinWidth="[float]1" Top="[float]0" VerticalAlignment="[VerticalAlignment]Bottom" Width="[float]NaN" />
+					<Path Data="[PathGeometry]M 2,0 C 2,1.10000002 1.10000002,2 0,2 L 0,0 L 2,0 Z" Fill="[SMSolidColorBrush]#ff00bff3" Height="[float]2" HorizontalAlignment="[HorizontalAlignment]Right" Id="ad7c1cd832544d03b8e6099a9b8edb30" IsHitTestVisible="[bool]False" Left="[float]0" Margin="[SMThickness]2,2,2,2" MinHeight="[float]1" MinWidth="[float]1" Top="[float]0" VerticalAlignment="[VerticalAlignment]Bottom" Width="[float]2" />
+					<Rectangle Fill="[SMSolidColorBrush]#ff00bff3" Height="[float]NaN" HorizontalAlignment="[HorizontalAlignment]Right" Id="bea2ca85e98f450492516f85ea48c993" IsHitTestVisible="[bool]False" Left="[float]0" Margin="[SMThickness]2,4,2,4" MinHeight="[float]1" MinWidth="[float]1" Top="[float]0" Width="[float]2" />
+					<Path Data="[PathGeometry]M 2,2 C 2,1.10000002 1.10000002,0 0,0 L 0,2 L 2,2 Z" Fill="[SMSolidColorBrush]#ff00bff3" Height="[float]2" HorizontalAlignment="[HorizontalAlignment]Right" Id="e60b1747aefe4ee18d917adcba2de378" IsHitTestVisible="[bool]False" Left="[float]0" Margin="[SMThickness]2,2,2,2" MinHeight="[float]1" MinWidth="[float]1" Top="[float]0" VerticalAlignment="[VerticalAlignment]Top" Width="[float]2" />
+					<Path Data="[PathGeometry]M 0,12 L 12,0 L 12,12 L 0,12 Z" Fill="[SMSolidColorBrush]#ff00bff3" Height="[float]12" HorizontalAlignment="[HorizontalAlignment]Right" Id="16de13595364d2d9808a4c6400c4eb3" IsHitTestVisible="[bool]False" Left="[float]0" Margin="[SMThickness]4,4,4,4" MinHeight="[float]1" MinWidth="[float]1" Top="[float]0" VerticalAlignment="[VerticalAlignment]Bottom" Width="[float]12" />
+				</IconTemplate>
+				<Path Data="[PathGeometry]M 31,10 L 0,10 L 0,0 L 3,0 L 3,5 L 4,5 L 4,0 L 6,0 L 6,3 L 7,3 L 7,0 L 9,0 L 9,5 L 10,5 L 10,0 L 12,0 L 12,3 L 13,3 L 13,0 L 15,0 L 15,5 L 16,5 L 16,0 L 18,0 L 18,3 L 19,3 L 19,0 L 21,0 L 21,5 L 22,5 L 22,0 L 24,0 L 24,3 L 25,3 L 25,0 L 27,0 L 27,5 L 28,5 L 28,0 L 31,0 L 31,10 Z" EditMode="[ShapeEditMode]FixedPath" Fill="[SMSolidColorBrush]#ff00bff3" Id="806fefcc312d452495e7d361ea404c07" Left="[float]3" Stroke="[SMSolidColorBrush]#00ffffff" Top="[float]5" Width="[float]31" />
+				<Path Data="[PathGeometry]M 19,0 C 22.1089993,0 22.9200001,2.76200008 23.4570007,4.59000015 C 24.0389996,6.57200003 24.3120003,7 25,7 L 26,7 L 26,10 L 25,10 C 21.9190006,10 21.1119995,7.25299978 20.5779991,5.43499994 C 19.9899998,3.43300009 19.7099991,3 19,3 C 18.2709999,3 18.0060005,3.40799999 17.4239998,5.42700005 C 16.8980007,7.24599981 16.1049995,9.99499989 13.0019999,10 L 12.9940004,10 C 9.88000011,10 9.10000038,7.25500011 8.58199978,5.43900013 C 8.00699997,3.41499996 7.73899984,3.00500011 6.98899984,3 L 6.97399998,3 C 6.30200005,3 6.02899981,3.31299996 5.42299986,5.41900015 C 4.89799976,7.24300003 4.10500002,10 1,10 L 0,10 L 0,7 L 1,7 C 1.69599998,7 1.97000003,6.57200003 2.53999996,4.59000015 C 3.06599998,2.76099992 3.86100006,0 6.97300005,0 L 7.01100016,0 C 10.1599998,0.0219999999 10.9469995,2.78699994 11.4680004,4.6170001 C 12.0600004,6.70100021 12.3269997,7 12.9940004,7 L 12.9980001,7 C 13.6960001,6.99900007 13.9709997,6.57200003 14.5410004,4.59499979 C 15.0690002,2.7650001 15.8660002,0 19,0" EditMode="[ShapeEditMode]FixedPath" Fill="[SMSolidColorBrush]#ff00bff3" Id="4d0866be16064bb6a0c59afff917ad99" Left="[float]6" Stroke="[SMSolidColorBrush]#00ffffff" Top="[float]18" Width="[float]26" />
+			</IconPanel>
+		</Icon>
+	</MatlabDefinition>
+	<EnvoyManagerFile Id="1c18212eaa7b4a7e9d258c8e00a51ab1" xmlns="http://www.ni.com/PlatformFramework">
+		<EnvoyManagerRootEnvoy Id="9d9e30cc7ab74c30af4d7a2eaab0f79b" ModelDefinitionType="EnvoyManagerRootEnvoy" Name="RootEnvoy" />
+		<NameScopingEnvoy AutomaticallyResolveUp="True" Id="aea8c9879d224523920470dc6706bd58" Name="Math_RSSI, SNR.mli" NameTracksFileName="True">
+			<EmbeddedDefinitionReference Id="30824e32a6e0437cbf3d07bf76678f21" ModelDefinitionType="{http://www.ni.com/Matlab}MatlabEntryPoint" Name="Math_RSSI, SNR">
+				<MatlabEntryPoint Id="f55439ade424457b5d49ccfc83425eb" xmlns="http://www.ni.com/Matlab">
+					<Icon Id="5e7259e53ce74ebf959a456d83ed1713" ListViewIconCrop="0 0 40 70" xmlns="http://www.ni.com/PlatformFramework">
+						<IconPanel Height="[float]70" Id="a4bdb70032bb4feaa5019a45886d8a14" Left="[float]0" PanelSizeMode="Resize" Top="[float]0" Width="[float]40">
+							<IconTemplate ClipMargin="[SMThickness]3,3,3,3" Height="[float]70" HorizontalAlignment="[HorizontalAlignment]Stretch" Id="1a2c85b7b53445eabb5ce68c2fd9abc7" Left="[float]0" TemplateName="[string]Corner" Top="[float]0" VerticalAlignment="[VerticalAlignment]Stretch" Width="[float]40">
+								<Rectangle Fill="[SMSolidColorBrush]#ff727272" Id="6a63658a34184787b62150cbb6f20c4f" IsHitTestVisible="[bool]False" Left="[float]0" RadiusX="[float]4" RadiusY="[float]4" Top="[float]0" />
+								<Rectangle Fill="[SMSolidColorBrush]#ffffffff" Id="9bfea5776b0a489fb7ad518e90efc7d1" IsHitTestVisible="[bool]False" Left="[float]0" Margin="[SMThickness]1,1,1,1" RadiusX="[float]2.5" RadiusY="[float]2.5" Stroke="[SMSolidColorBrush]#ffffffff" Top="[float]0" />
+								<Path Data="[PathGeometry]M 0,0 C 0,1.10000002 0.899999976,2 2,2 L 2,0 L 0,0 Z" Fill="[SMSolidColorBrush]#ff00a4e5" Height="[float]2" HorizontalAlignment="[HorizontalAlignment]Left" Id="d14cdaefff8542008647951c4b22a99c" IsHitTestVisible="[bool]False" Left="[float]0" Margin="[SMThickness]2,2,2,2" MinHeight="[float]1" MinWidth="[float]1" Top="[float]0" VerticalAlignment="[VerticalAlignment]Bottom" Width="[float]2" />
+								<Rectangle Fill="[SMSolidColorBrush]#ff00a4e5" Height="[float]2" Id="310d4da56ec1431f8e4e52870d3398a3" IsHitTestVisible="[bool]False" Left="[float]0" Margin="[SMThickness]4,2,4,2" MinHeight="[float]1" MinWidth="[float]1" Top="[float]0" VerticalAlignment="[VerticalAlignment]Bottom" Width="[float]NaN" />
+								<Path Data="[PathGeometry]M 2,0 C 2,1.10000002 1.10000002,2 0,2 L 0,0 L 2,0 Z" Fill="[SMSolidColorBrush]#ff00a4e5" Height="[float]2" HorizontalAlignment="[HorizontalAlignment]Right" Id="3c28ba9806c44bf3849d00338c9bc3e3" IsHitTestVisible="[bool]False" Left="[float]0" Margin="[SMThickness]2,2,2,2" MinHeight="[float]1" MinWidth="[float]1" Top="[float]0" VerticalAlignment="[VerticalAlignment]Bottom" Width="[float]2" />
+								<Rectangle Fill="[SMSolidColorBrush]#ff00a4e5" Height="[float]NaN" HorizontalAlignment="[HorizontalAlignment]Right" Id="3597b2b1b6b4236a3f177bd166f2b6d" IsHitTestVisible="[bool]False" Left="[float]0" Margin="[SMThickness]2,4,2,4" MinHeight="[float]1" MinWidth="[float]1" Top="[float]0" Width="[float]2" />
+								<Path Data="[PathGeometry]M 2,2 C 2,1.10000002 1.10000002,0 0,0 L 0,2 L 2,2 Z" Fill="[SMSolidColorBrush]#ff00a4e5" Height="[float]2" HorizontalAlignment="[HorizontalAlignment]Right" Id="92bbe5da7c56444cb93b608642edb437" IsHitTestVisible="[bool]False" Left="[float]0" Margin="[SMThickness]2,2,2,2" MinHeight="[float]1" MinWidth="[float]1" Top="[float]0" VerticalAlignment="[VerticalAlignment]Top" Width="[float]2" />
+								<Path Data="[PathGeometry]M 0,12 L 12,0 L 12,12 L 0,12 Z" Fill="[SMSolidColorBrush]#ff00a4e5" Height="[float]12" HorizontalAlignment="[HorizontalAlignment]Right" Id="e6e19b9b89514d7bbf73584fd38b134f" IsHitTestVisible="[bool]False" Left="[float]0" Margin="[SMThickness]4,4,4,4" MinHeight="[float]1" MinWidth="[float]1" Top="[float]0" VerticalAlignment="[VerticalAlignment]Bottom" Width="[float]12" />
+							</IconTemplate>
+							<Path Data="[PathGeometry]M 1,0 L 1,4 L 1.70299995,4 C 0.672999978,4.82499981 0,6.07800007 0,7.5 C 0,9.98499966 2.0150001,12 4.5,12 C 5.92500019,12 7.1789999,11.3249998 8.00399971,10.2919998 L 11,15 L 11,25 L 16,21 L 16,15 L 23,4 L 26,4 L 26,0 L 1,0 Z M 4.5,10 C 3.12199998,10 2,8.87800026 2,7.5 C 2,6.12099981 3.12199998,5 4.5,5 C 4.55000019,5 4.59700012,5.01200008 4.64599991,5.01499987 L 6.81799984,8.42700005 C 6.44799995,9.34700012 5.55000019,10 4.5,10 Z" EditMode="[ShapeEditMode]FixedPath" Fill="[SMSolidColorBrush]#ff00a4e5" Height="[float]25" Id="b592638601db414e8f24e3d3081e29d1" Left="[float]7" Stroke="[SMSolidColorBrush]#00ffffff" Top="[float]30" Width="[float]26" />
+							<Path Data="[PathGeometry]M 19,0 C 22.1089993,0 22.9200001,2.76200008 23.4570007,4.59000015 C 24.0389996,6.57200003 24.3120003,7 25,7 L 26,7 L 26,10 L 25,10 C 21.9190006,10 21.1119995,7.25299978 20.5779991,5.43499994 C 19.9899998,3.43300009 19.7099991,3 19,3 C 18.2709999,3 18.0060005,3.40799999 17.4239998,5.42700005 C 16.8980007,7.24599981 16.1049995,9.99499989 13.0019999,10 L 12.9940004,10 C 9.88000011,10 9.10000038,7.25500011 8.58199978,5.43900013 C 8.00699997,3.41499996 7.73899984,3.00500011 6.98899984,3 L 6.97399998,3 C 6.30200005,3 6.02899981,3.31299996 5.42299986,5.41900015 C 4.89799976,7.24300003 4.10500002,10 1,10 L 0,10 L 0,7 L 1,7 C 1.69599998,7 1.97000003,6.57200003 2.53999996,4.59000015 C 3.06599998,2.76099992 3.86100006,0 6.97300005,0 L 7.01100016,0 C 10.1599998,0.0219999999 10.9469995,2.78699994 11.4680004,4.6170001 C 12.0600004,6.70100021 12.3269997,7 12.9940004,7 L 12.9980001,7 C 13.6960001,6.99900007 13.9709997,6.57200003 14.5410004,4.59499979 C 15.0690002,2.7650001 15.8660002,0 19,0" EditMode="[ShapeEditMode]FixedPath" Fill="[SMSolidColorBrush]#ff00bff3" Id="38485d462c6453ab75fef52ddfd2a1e" Left="[float]7" Stroke="[SMSolidColorBrush]#00ffffff" Top="[float]14" Width="[float]26" />
+						</IconPanel>
+					</Icon>
+					<ConnectorPane Height="70" Id="e8f8efcdf77342f3b6f754b01d9c2ac6" ListViewHeight="115" ListViewWidth="150" Width="40" xmlns="http://www.ni.com/PlatformFramework">
+						<ConnectorPaneTerminal ListViewHotspot="0 -10" Parameter="585e3adad65a4836923bc64d1d7fef52" />
+						<ConnectorPaneTerminal Hotspot="0 15" ListViewHotspot="0 -10" Parameter="da0d3c8c3abf421b94fef44bd3c09901" />
+						<ConnectorPaneTerminal Hotspot="0 25" />
+						<ConnectorPaneTerminal Hotspot="0 35" />
+						<ConnectorPaneTerminal Hotspot="15 0" />
+						<ConnectorPaneTerminal Hotspot="25 0" />
+						<ConnectorPaneTerminal Hotspot="40 5" />
+						<ConnectorPaneTerminal Hotspot="40 15" />
+						<ConnectorPaneTerminal Hotspot="40 25" ListViewHotspot="150 20" Parameter="7cdf6e476b4640d4af8ffdad8b254c1b" />
+						<ConnectorPaneTerminal Hotspot="40 35" ListViewHotspot="150 35" Parameter="727f9998b1f04997855cd89633aa41b2" />
+						<ConnectorPaneTerminal Hotspot="15 70" />
+						<ConnectorPaneTerminal Hotspot="25 70" />
+						<ConnectorPaneTerminal Hotspot="0 45" />
+						<ConnectorPaneTerminal Hotspot="40 45" ListViewHotspot="150 50" Parameter="c4ab71b1ec2440b39d4e30beea79ed73" />
+						<ConnectorPaneTerminal Hotspot="0 55" />
+						<ConnectorPaneTerminal Hotspot="40 55" ListViewHotspot="150 65" Parameter="7c9fee9464a543d382d40c771b8e03cc" />
+						<ConnectorPaneTerminal Hotspot="0 65" ListViewHotspot="0 -10" Parameter="e504588eaaf6470faf49a7ee626c1bd8" />
+						<ConnectorPaneTerminal Hotspot="40 65" ListViewHotspot="150 5" Parameter="ea4904107f674c8e9bedcee735d8c615" />
+					</ConnectorPane>
+					<MatlabErrorDiagramParameter CallDirection="Input" Id="e504588eaaf6470faf49a7ee626c1bd8" Name="에러 입력" />
+					<MatlabErrorDiagramParameter CallDirection="Output" Id="ea4904107f674c8e9bedcee735d8c615" Name="에러 출력" />
+					<MatlabParameter DataType="Double[]" Id="bcf30c00bcb2479ab0d5a14b8fa04253" Name="Power">
+						<MatlabDiagramParameter CallDirection="Input" Id="585e3adad65a4836923bc64d1d7fef52" Name="Power" />
+						<MatlabDiagramParameter CallDirection="Output" Id="72add8251a45413da7f577ac72d762c5" Name="Power out" Visible="False" />
+					</MatlabParameter>
+					<MatlabParameter DataType="Int32" Id="ae874ab6f10c4d4b9d7107ccb7ea175f" Name="bin">
+						<MatlabDiagramParameter CallDirection="Input" Id="da0d3c8c3abf421b94fef44bd3c09901" Name="bin" />
+						<MatlabDiagramParameter CallDirection="Output" Id="5c55d7ce3dc34314af7ce2741c1d6655" Name="bin out" Visible="False" />
+					</MatlabParameter>
+					<MatlabParameter DataType="Double" Id="dff967a8be3d48829b1dea53968f25ab" Name="noise_power">
+						<MatlabDiagramParameter CallDirection="Input" Id="3014d6b0fea14eec8bec79b5a7857334" Name="noise_power in" Visible="False" />
+						<MatlabDiagramParameter CallDirection="Output" Id="7cdf6e476b4640d4af8ffdad8b254c1b" Name="noise_power" />
+					</MatlabParameter>
+					<MatlabParameter DataType="Double" Id="17f98ae9c1244053a160f4af93668612" Name="energy_threshold">
+						<MatlabDiagramParameter CallDirection="Input" Id="9dc1c0725e3b4fc7ac5b67246dab9348" Name="energy_threshold in" Visible="False" />
+						<MatlabDiagramParameter CallDirection="Output" Id="727f9998b1f04997855cd89633aa41b2" Name="energy_threshold" />
+					</MatlabParameter>
+					<MatlabParameter DataType="Double" Id="7b4a90c60fee413abb75d4c90c3cb876" Name="signal_power">
+						<MatlabDiagramParameter CallDirection="Input" Id="3465620fce644ec6be82ecb48ae588d4" Name="signal_power in" Visible="False" />
+						<MatlabDiagramParameter CallDirection="Output" Id="c4ab71b1ec2440b39d4e30beea79ed73" Name="signal_power" />
+					</MatlabParameter>
+					<MatlabParameter DataType="Int32" Id="fd2f05d1d91644b59cbf02bd1de0a567" Name="noise_estimation_error">
+						<MatlabDiagramParameter CallDirection="Input" Id="e6b44bee6c1d416e8799ed8e7fa1cffd" Name="noise_estimation_error in" Visible="False" />
+						<MatlabDiagramParameter CallDirection="Output" Id="7c9fee9464a543d382d40c771b8e03cc" Name="noise_estimation_error" />
+					</MatlabParameter>
+				</MatlabEntryPoint>
+			</EmbeddedDefinitionReference>
+		</NameScopingEnvoy>
+	</EnvoyManagerFile>
+</SourceFile>
